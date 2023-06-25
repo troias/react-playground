fe0b4cfd6928e3d318788dcfa55c997bc3a6e2f0
troias <hunter007nz@gmail.com>
2023-06-26 00:04:03 +1200

    added programtic handler

--- src/components/eventItem.js
+++ src/components/eventItem.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { useLoaderData, Link } from "react-router-dom";
+import { useLoaderData, Link, useSubmit } from "react-router-dom";
 
 export default function EventItem() {
   const data = useLoaderData();
+  const submit = useSubmit();
   const { id, title, description, year } = data;
+
+  const deleteHandler = () => {
+    console.log("Delete");
+
+    const proceed = window.confirm(
+      "Are you sure you want to delete this item?"
+    );
+    if (proceed) {
+      console.log("Delete confirmed");
+      submit(null, {
+        method: "delete",
+        url: `/api/events/${id}`,
+      });
+    }
+  };
+
   return (
     <div>
       <h2>{title}</h2>
@@ -12,6 +29,19 @@ export default function EventItem() {
       <Link to={`/events/edit/${id}`} params={{ id }}>
         Edit
       </Link>
+      <button onClick={deleteHandler}>Delete</button>
     </div>
   );
+}
+
+export async function action({ params, fetch }) {
+  const data = await fetch(`/api/events/${params.id}`).then((res) =>
+    res.json()
+  );
+
+  return {
+    title: "Event",
+    component: <EventItem />,
+    data,
+  };
 }
